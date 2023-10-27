5a6672edc24f3722d84dcfe057e7fcce9bc11697
edgar <edgar99carbajal@gmail.com>
2023-10-27 10:45:01 -0700

    set up base, button doesnt move

--- Assets/ButtonTests/ButtonClick.cs
+++ Assets/ButtonTests/ButtonClick.cs
@@ -15,16 +15,11 @@ public class ButtonClick : MonoBehaviour
 
     void Start()
     {
-        //canvas = GetComponent<Canvas>(); //get the canvas
-        //ourButton = GameObject.Find("ButtonTest").GetComponent<Button>(); //get the button
-        //newText = GameObject.Find("TextTest").GetComponent<TextMeshProUGUI>();
-        //ourCanvas = GameObject.Find("TestCanvas").GetComponent<Canvas>();
-
 
         buttonPosition = ourButton.transform.position; //getting the position of the button
 
         Debug.Log("button pos: " + ourButton.transform.position);
-    }
+    }  
 
     //what to do when button is clicked
     public void buttonClicked()
@@ -40,24 +35,6 @@ public class ButtonClick : MonoBehaviour
         Debug.Log("Clicked!");
 
 
-        //GameObject gO = new GameObject();
-
-        //TextMeshProUGUI newText = gO.AddComponent<TextMeshProUGUI>();
-
-
-        //newText.text = "Les gooo"; //making a text and font
-        //newText.fontSize = 30.0f;
-
-        //float goX = gO.transform.GetComponent<RectTransform>().localScale.x;
-        //float goY = gO.transform.GetComponent<RectTransform>().localScale.y;
-        //float goZ = gO.transform.GetComponent<RectTransform>().localScale.z;
-
-
-        //gO.transform.SetParent(ourButton.transform);
-
-        //gO.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        //gO.transform.localPosition = new Vector3(buttonPosition.x +30.0f, buttonPosition.y + 150.0f, buttonPosition.z); //text is above the button
-        //Debug.Log("text posk: " + gO.transform.position + " text: " + newText.text);
 
     } 
 }
