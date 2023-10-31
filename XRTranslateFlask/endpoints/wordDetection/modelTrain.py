edff928bf03237f93a98b4fd7fd4376264f0c2a3
edgar <edgar99carbajal@gmail.com>
2023-10-31 13:34:39 -0700

    regorganized

---
+++ XRTranslateFlask/endpoints/wordDetection/modelTrain.py
@@ -0,0 +1,7 @@
+import os
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+print("TensorFlow version: {}".format(tf.__version__))
+print("TensorFlow Datasets version: ",tfds.__version__)
