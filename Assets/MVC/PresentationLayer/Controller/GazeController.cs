2fc1b85e871bf5d59e528147d322de4d99a9dbbf
ethan smith <ethansmith.s@icloud.com>
2024-01-24 13:22:09 -0800

    im wretten a message

--- Assets/MVC/PresentationLayer/Controller/GazeController.cs
+++ Assets/MVC/PresentationLayer/Controller/GazeController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ModifyText : MonoBehaviour
+public class GazeController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
